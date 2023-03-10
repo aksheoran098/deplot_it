beb1585fff5bcc3d5d2ea77d5186790b3e6e3ee5
Ankush Sheoran <ankushsheoran@Ankushs-MacBook-Air.local>
2023-03-10 23:09:06 +0530

    3rd try

--- database.js
+++ database.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
 
-const connectDatabase = ()=> {
-    mongoose.connect(process.env.DB_URI ).then(
-        (data) => {
-            console.log(`MongoDb connected with server: ${data.connection.host}`);
-        }
-    ).catch(
-        (err) => {console.log(err)}
-    )
-}
+const connectDatabase = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.DB_URI);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
 
 module.exports = connectDatabase;
